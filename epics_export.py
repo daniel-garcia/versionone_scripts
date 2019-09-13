b8527d55316fcf1d3fab8d71cfe06ac94bd1dbae
Daniel Garcia <daniel@danielgarcia.info>
2019-09-13 01:31:45 -0500

    add script to export epics for program review

---
+++ epics_export.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import os
+import json
+import argparse
+import requests
+from dateutil.parser import parse as dateparse
+
+
+def query(scope, filter):
+    q = """
+{
+  "from": "Epic",
+  "select": [
+    "ID.Name",
+    "Name",
+    "Number",
+    "Custom_TSAStatus2.Name",
+    "Custom_TSADate"
+  ],
+  "sort": [
+    "+Order"
+  ],
+  "where": {
+    "Scope.Name": "%s",
+    "Custom_TSAStatus2.Name": "%s"
+  }
+}
+""" % (scope, filter)
+    url = args.endpoint + '/query.v1'
+    req = requests.post(url, data=q, headers=headers)
+    stories = req.json()[0]
+    i = 0
+    for i in range(len(stories)):
+        s = stories[i]
+        if filter == "Required":
+            ds = s['Custom_TSADate']
+            if ds:
+                ds = dateparse(ds)
+                ds = ds.strftime('%m/%d/%Y')
+            else:
+                ds = "TBD"
+            print ("%s, %s - Target %s" % (s['Number'],s['Name'], ds))
+        else:
+            print ("%s, %s" % (s['Number'],s['Name']))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='export versionone stories.')
+    parser.add_argument('--token', default=os.environ.get('VERSION_ONE_TOKEN'))
+    parser.add_argument('--endpoint', default=os.environ.get('VERSION_ONE_ENDPOINT'))
+    parser.add_argument("tsa_status", default="")
+    args = parser.parse_args()
+    headers = {}
+
+    if args.token:
+        headers['Content-Type'] = 'application/json'
+        headers['Authorization'] = 'Bearer ' + args.token
+
+    query("Atlas 2.6", args.tsa_status)
+
+
